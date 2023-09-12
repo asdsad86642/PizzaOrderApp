84a07d4836c9f9b3d95af018c333580b310a2789
asdsad86642 <asdsad86642@gmail.com>
2023-09-12 19:18:26 +0900

    탭레이아웃 + 뷰페이저 배치

---
+++ app/src/main/java/com/example/pizzaorderapp/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pizzaorderapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity  :  AppCompatActivity(){
+
+    val mContext = this
+
+   abstract fun setupEvents()
+   abstract fun setValues()
+
+
+}
--- app/src/main/java/com/example/pizzaorderapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp/MainActivity.kt
@@ -4,12 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pizzaorderapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val  view = binding.root
         setContentView(view)
+        setupEvents()
+        setValues()
     }
+
+
+
+
+    override fun setupEvents() {
+
+    }
+
+    //버튼 클릭등의 이벤트 관련 코드 모음.
+
+    override fun setValues() {
+
+//        binding.mainTapLayout.setupWithViewPager()
+
+        // 화면에 데이터를 표시하기 위한 코드 모음.
+
+    }
+
+
+
+
 }
