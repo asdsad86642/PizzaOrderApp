ebc0b1b80b55e3a6367d3b4605a1a134a825aac9
asdsad86642 <asdsad86642@gmail.com>
2023-09-13 14:46:15 +0900

    가게 상세보기 화면 추가

--- app/src/main/java/com/example/pizzaorderapp/datas/Store.kt
+++ app/src/main/java/com/example/pizzaorderapp/datas/Store.kt
@@ -2,5 +2,8 @@ package com.example.pizzaorderapp.datas
 
 class Store(val name : String,
             val phoneNumber : String ,
-            val  logoUrl : String) {
-}
+            val  logoUrl : String,
+            )
+
+
+
--- app/src/main/java/com/example/pizzaorderapp/fragmnet/PizzaStoreFragment.kt
+++ app/src/main/java/com/example/pizzaorderapp/fragmnet/PizzaStoreFragment.kt
@@ -34,10 +34,10 @@ class PizzaStoreFragment : Fragment() {
         //동작 관련 코드
 
 
-        mPizzaStoreDataList.add(Store("피자스쿨","010-1456-8756","www.naver.com"))
-        mPizzaStoreDataList.add(Store("파파존스","010-1456-8756","www.daum.net"))
-        mPizzaStoreDataList.add(Store("김명숙피자","010-1456-8756","www.google.com"))
-        mPizzaStoreDataList.add(Store("치킨나라 피자공주","010-1456-8756","www.facebook.com"))
+        mPizzaStoreDataList.add(Store("피자스쿨","010-1456-8756","https://cdn3.iconfinder.com/data/icons/street-food-and-food-trucker-1/64/pizza-fast-food-bake-bread-128.png"))
+        mPizzaStoreDataList.add(Store("파파존스","010-1456-8756","https://cdn1.iconfinder.com/data/icons/cartoon-snack/128/pizza-128.png"))
+        mPizzaStoreDataList.add(Store("김명숙피자","010-1456-8756","https://cdn3.iconfinder.com/data/icons/food-set-3/91/Food_C219-128.png"))
+        mPizzaStoreDataList.add(Store("치킨나라 피자공주","010-1456-8756","https://cdn0.iconfinder.com/data/icons/fastfood-31/64/pizza-italian-food-fast-fastfood-cheese-piece-128.png"))
 
         mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(),R.layout.pizza_store_list_item,mPizzaStoreDataList)
 
